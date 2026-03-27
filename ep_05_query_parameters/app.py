afd65322ea738cd6e50f022000e3e6c640959ed5
ZeqTech <159857785+ZeqTech@users.noreply.github.com>
2026-03-27 16:08:05 -0500

    Added missing examples for episode 4

--- ep_05_query_parameters/app.py
+++ ep_05_query_parameters/app.py
@@ -4,6 +4,7 @@
 # - uvicorn app:app --reload
 # - go to http://localhost:8000/docs
 
+from enum import Enum
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -19,3 +20,22 @@ async def get_items(count: int = 2, name: str = None):
 @app.get("/item/{item_id}")
 async def get_item(item_id: int, count: int = 2, name: str = None):
     return {"item_id": item_id, "count": count, "name": name}
+
+
+# With List Query Parameters
+@app.get("/items")
+async def get_item_list(items: list[str] | None = None):
+    return {"items": items}
+
+
+# With Enum Query Parameters
+class Fruit(Enum):
+    APPLE = "apple"
+    BANANA = "banana"
+    PEAR = "pear"
+
+
+@app.get("/fruits/")
+async def read_items(fruit: Fruit):
+    query_items = {"fruit": fruit.value}
+    return query_items
